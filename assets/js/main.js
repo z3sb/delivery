bceac772d0c48c65c2eb49413b56a3444eca48ad
z3sb <oslvuao@gmail.com>
2022-09-20 16:09:49 +0300

    add function to check the active section

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,11 +20,32 @@ function linkAction(){
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
 /* ================== Scroll Section Active Link ================== */
-
-
+const sections = document.querySelectorAll('section[id]')
+function scrollActive(){
+  const scrollY = window.pageYOffset;
+  sections.forEach(current => {
+    const 
+      sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 50,
+      sectionId = current.getAttribute('id');
+    if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+      if(document.querySelector('.nav__menu a[href*="#' + sectionId + '"]')){
+        document.querySelector('.nav__menu a[href*="#' + sectionId + '"]').classList.add('active-link');
+      }
+    } else{
+      if(document.querySelector('.nav__menu a[href*="#' + sectionId + '"]')){
+        document.querySelector('.nav__menu a[href*="#' + sectionId + '"]').classList.remove('active-link');
+      }
+    }
+  })
+}
+window.addEventListener('scroll', scrollActive)
 /* ================== Change Background Header ================== */
-
-
+function scrollHeader() {
+  const header = document.getElementById('header');
+  if(this.scrollY >= 80) header.classList.add('scroll-header'); else header.classList.remove('scroll-header');
+}
+window.addEventListener('scroll', scrollHeader)
 /* ================== Show Scroll up ================== */
 
 
